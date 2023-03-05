458806ee0d1790cf7534cbb243f587418ab4efa8
abdul-rehman-ship <abd22655@gmail.com>
2023-03-05 21:39:44 +0500

    commit

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import type { ReactElement } from 'react'
 import Head from 'next/head'
 import BaseButton from '../components/BaseButton'
-import CardBox from '../components/CardBox'
+
 import SectionFullScreen from '../components/SectionFullScreen'
 import LayoutGuest from '../layouts/Guest'
 import { getPageTitle } from '../config'
@@ -15,16 +15,9 @@ export default function Error() {
       </Head>
 
       <SectionFullScreen bg="pinkRed">
-        <CardBox
-          className="w-11/12 md:w-7/12 lg:w-6/12 xl:w-4/12 shadow-2xl"
-          footer={<BaseButton href="/dashboard" label="Done" color="danger" />}
-        >
-          <div className="space-y-3">
-            <h1 className="text-2xl">Unhandled exception</h1>
+       
+        <p>An Error Occurred</p>
 
-            <p>An Error Occurred</p>
-          </div>
-        </CardBox>
       </SectionFullScreen>
     </>
   )
